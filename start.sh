ff7899045c8b5adc2c743b6c570dce64bb4f74b0
Israel-Lopes <yisrael.lopes@gmail.com>
2023-04-07 11:09:46 -0300

    modules

--- start.sh
+++ start.sh
@@ -7,3 +7,24 @@ source config/directories_path.sh
 source utils/painel.sh
 # Funcao para criar diretorios
 source utils/mk.sh
+
+# Titulo do programa
+_print_painel
+
+# Criação de diretórios
+_create_directory "$INBOUND_PATH"
+_create_directory "$ENTITY_PATH"
+_create_directory "$MAPPER_PATH"
+_create_directory "$REQUEST_PATH"
+_create_directory "$OUTBOUND_PATH"
+_create_directory "$REPOSITORY_PATH"
+_create_directory "$REST_PATH"
+_create_directory "$DOMAIN_PATH"
+_create_directory "$EXCEPTION_PATH"
+_create_directory "$SERVICE_PATH"
+_create_directory "$IN_PATH"
+_create_directory "$OUT_PATH"
+_create_directory "$CONFIG_PATH"
+_create_directory "$RESOURCES"
+
+source modules/create_content_file/create_content_entity.sh
